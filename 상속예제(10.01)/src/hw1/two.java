a91fd3c3a37659cb3e25142a9ca31d444849e7b2
cksl0830 <60590737+cksl0830@users.noreply.github.com>
2020-10-03 17:57:15 +0900

    hw

---
+++ 상속예제(10.01)/src/hw1/two.java
@@ -0,0 +1,35 @@
+package hw1;
+
+class Person{
+	   private int weight;
+	   int age;
+	   protected int height;
+	   public String name;
+	   
+	   public void setWeight(int weight) {
+	      this.weight = weight;
+	   }
+	   
+	   public int getWeight() {
+	      return weight;
+	   }
+	}
+
+	class Student extends Person{
+	   public void set() {
+	      age = 30;
+	      name = "gildong";
+	      height = 180;
+	      setWeight(99);
+	   }
+	}
+
+	public class two {
+
+	   public static void main(String[] args) {
+	      Student s = new Student();
+	      s.set();
+	   }
+	}
+
+
